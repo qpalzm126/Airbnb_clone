26877c266ca1e9181b492ab5719422942d68660c
LLChen <qpalzm126@gmail.com>
2023-02-24 13:10:34 +0800

    20230224

--- components/Header.js
+++ components/Header.js
@@ -10,16 +10,32 @@ import {
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
-function Header() {
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoofGuests] = useState(1);
-
+  const router = useRouter();
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+  const search = () => {
+    router.push({
+      pathname: "search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
   };
 
   const selectionRange = {
@@ -29,7 +45,10 @@ function Header() {
   };
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto ">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto "
+      >
         <Image
           src="https://links.papareact.com/qd3"
           fill
@@ -41,7 +60,7 @@ function Header() {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
-          placeholder="start search"
+          placeholder={placeholder || "start search"}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-700 placeholder-gray-400  "
         />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
@@ -74,6 +93,14 @@ function Header() {
               min={1}
               className="w-12 pl-2 text-lg outline-none text-red-400"
             />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
           </div>
         </div>
       )}
---
+++ components/InfoCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InfoCard({ img, location, title, description, star, price, total }) {
+  return <div></div>;
+}
+
+export default InfoCard;
