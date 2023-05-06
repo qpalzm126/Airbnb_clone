acc38a1129bd9ef24a26977f7e7aa6e58f4bc12b
LLChen <qpalzm126@gmail.com>
2023-05-07 03:47:46 +0800

    20230507 0347

--- components/Banner.js
+++ components/Banner.js
@@ -3,18 +3,20 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[300] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+    <div className="relative  h-[200px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image
         src="https://links.papareact.com/0fm"
         fill
         className="object-cover"
       />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <p className="text-sm sm:text-lg over">
+          Not sure where to go? Perfect.
+        </p>
         <button
-          className="text-purple-500 bg-white px-10 py-4 shadow-md 
+          className="text-fuchsia-600 bg-white px-10 py-4 shadow-md 
         rounded-full font-bold my-3 hover:shadow-xl active:scale-90 
-        transition duration-150"
+        transition duration-150 max-sm:text-sm max-sm:px-5 max-sm:py-4"
         >
           Start
         </button>
--- components/MediumCard.js
+++ components/MediumCard.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function MediumCard({ img, title }) {
   return (
     <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
-      <div className="relative h-80 w-80">
+      <div className="relative h-80 w-80 max-md:h-40 max-md:w-40">
         <Image src={img} fill className="rounded-xl" />
       </div>
       <h3 className="text-2xl mt-3">{title}</h3>
